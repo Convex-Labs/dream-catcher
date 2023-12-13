a64d6cc853f33756e6054c20489d25111c429158
Ricardo Rosales <rrosales1028@gmail.com>
2023-12-12 22:19:42 -0800

    Generated by Aineko Dream

---
+++ number_doubler/nodes.py
@@ -0,0 +1,23 @@
+from aineko.core.node import AbstractNode
+
+
+class NumberGeneratorNode(AbstractNode):
+    def _pre_loop_hook(self, params=None):
+        self.start = params.get('start', 0)
+        self.end = params.get('end', 10)
+        self.current = self.start
+
+    def _execute(self, params=None):
+        if self.current <= self.end:
+            self.producers['sequence'].produce(self.current)
+            self.current += 1
+        else:
+            self.activate_poison_pill()
+
+
+class NumberDoublerNode(AbstractNode):
+    def _execute(self, params=None):
+        msg = self.consumers['sequence'].consume(how='next')
+        if msg is not None:
+            doubled_value = msg * 2
+            self.producers['doubled_sequence'].produce(doubled_value)
