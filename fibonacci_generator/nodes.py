e04901db817ae508ca4703a28ee27f6dbf70cf69
convex-engineering <129983380+convex-engineering@users.noreply.github.com>
2023-12-13 20:47:19 -0500

    Generated by Aineko Dream

---
+++ fibonacci_generator/nodes.py
@@ -0,0 +1,16 @@
+from aineko.internals.node import AbstractNode
+from typing import Optional
+
+class FibonacciProducer(AbstractNode):
+    def _pre_loop_hook(self, params: Optional[dict] = None) -> None:
+        self.state = {'a': 0, 'b': 1}
+
+    def _execute(self, params: Optional[dict] = None) -> None:
+        a, b = self.state['a'], self.state['b']
+        self.producers['fibonacci_sequence'].produce(a)
+        self.state['a'], self.state['b'] = b, a + b
+
+class FibonacciConsumer(AbstractNode):
+    def _execute(self, params: Optional[dict] = None) -> None:
+        fibonacci_number = self.consumers['fibonacci_sequence'].next()
+        self.log(f'Received Fibonacci number: {fibonacci_number}', level='info')
