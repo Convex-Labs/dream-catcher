4bc86ea20eb4227adf709faf9ef9b9d272de1ae3
convex-engineering <129983380+convex-engineering@users.noreply.github.com>
2023-12-18 18:00:42 -0500

    Generated by Aineko Dream

---
+++ data_processor/nodes.py
@@ -0,0 +1,14 @@
+from aineko.core.node import AbstractNode
+from typing import Optional
+
+class DataTransformer(AbstractNode):
+
+    def _pre_loop_hook(self, params: Optional[dict] = None):
+        """Optional; used to initialize node state."""
+        self.transformation_rule = params.get("transformation_rule", lambda x: x)
+
+    def _execute(self, params: Optional[dict] = None):
+        """Required; function repeatedly executes."""
+        input_data = self.consumers["input_dataset"].next()
+        transformed_data = self.transformation_rule(input_data)
+        self.producers["output_dataset"].produce(transformed_data)
